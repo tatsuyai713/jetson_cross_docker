123b086f2dd48e4f18ca0106ad6fa0126e2903b9
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2021-12-19 17:30:30 +0000

    initial commit

---
+++ launch_container_no_nvidia.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+
+NAME_IMAGE='jetson_cross_ws'
+
+if [ ! "$(docker image ls -q ${NAME_IMAGE})" ]; then
+	if [ ! $# -ne 1 ]; then
+		if [ "setup" = $1 ]; then
+			echo "Image ${NAME_IMAGE} does not exist."
+			echo 'Now building image without proxy...'
+			docker build --file=./noproxy.dockerfile -t $NAME_IMAGE . --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=$USER
+			
+		else
+			echo "Docker image not found. Please setup first!"
+			exit
+		fi
+	else
+		echo "Docker image not found. Please setup first!"
+		exit
+  	fi
+fi
+
+# Commit
+if [ ! $# -ne 1 ]; then
+	if [ "commit" = $1 ]; then
+		echo 'Now commiting docker container...'
+		docker commit jetson_cross_docker jetson_cross_ws:latest
+		CONTAINER_ID=$(docker ps -a -f name=jetson_cross_docker --format "{{.ID}}")
+		docker rm $CONTAINER_ID
+		exit
+	fi
+fi
+
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+if [ ! -z "$xauth_list" ];  then
+  echo $xauth_list | xauth -f $XAUTH nmerge -
+fi
+chmod a+r $XAUTH
+
+DOCKER_OPT=""
+DOCKER_NAME="jetson_cross_docker"
+DOCKER_WORK_DIR="/home/${USER}"
+
+## For XWindow
+DOCKER_OPT="${DOCKER_OPT} \
+        --env=QT_X11_NO_MITSHM=1 \
+        --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw \
+        --volume=/home/${USER}:/home/${USER}/host_home:rw \
+        --env=XAUTHORITY=${XAUTH} \
+        --volume=${XAUTH}:${XAUTH} \
+        --env=DISPLAY=${DISPLAY} \
+        -w ${DOCKER_WORK_DIR} \
+        -u ${USER} \
+        --hostname `hostname`-Docker \
+        --add-host `hostname`-Docker:127.0.1.1"
+
+# For nvidia-docker
+# DOCKER_OPT="${DOCKER_OPT} --runtime=nvidia "
+# DOCKER_OPT="${DOCKER_OPT} --privileged -it "
+
+## Allow X11 Connection
+xhost +local:`hostname`-Docker
+CONTAINER_ID=$(docker ps -a -f name=jetson_cross_docker --format "{{.ID}}")
+if [ ! "$CONTAINER_ID" ]; then
+	docker run ${DOCKER_OPT} \
+		--volume=/dev:/dev:rw \
+		--shm-size=1gb \
+		--env=TERM=xterm-256color \
+		--net=host \
+		--name=${DOCKER_NAME} \
+		jetson_cross_ws:latest \
+		bash
+else
+	docker start $CONTAINER_ID
+	docker attach $CONTAINER_ID
+fi
+
+xhost -local:`hostname`-Docker
---
+++ noproxy.dockerfile
@@ -0,0 +1,61 @@
+FROM nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04
+
+ENV NVIDIA_VISIBLE_DEVICES ${NVIDIA_VISIBLE_DEVICES:-all}
+ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}graphics
+
+ARG UID=9001
+ARG GID=9001
+ARG UNAME=tatsuyai
+ARG HOSTNAME=docker
+
+ARG NEW_HOSTNAME=${HOSTNAME}-Docker
+
+ARG USERNAME=$UNAME
+ARG HOME=/home/$USERNAME
+RUN useradd -u $UID -m $USERNAME && \
+        echo "$USERNAME:$USERNAME" | chpasswd && \
+        usermod --shell /bin/bash $USERNAME && \
+        usermod -aG sudo $USERNAME && \
+        mkdir /etc/sudoers.d && \
+        echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$USERNAME && \
+        chmod 0440 /etc/sudoers.d/$USERNAME && \
+        usermod  --uid $UID $USERNAME && \
+        groupmod --gid $GID $USERNAME && \
+        chown -R $USERNAME:$USERNAME $HOME
+
+# install package
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && apt-get install -y --no-install-recommends \
+        build-essential \
+        curl \
+        sudo \
+        less \
+        apt-utils \
+        tzdata \
+        git \
+        tmux \
+        bash-completion \
+        command-not-found \
+        libglib2.0-0 \
+        gstreamer1.0-plugins-* \
+        libgstreamer1.0-* \
+        libgstreamer-plugins-*1.0-* \
+        vim \
+        emacs \
+        ssh \
+        rsync \
+        python-pip \
+        sed \
+        ca-certificates \
+        wget \
+        lsb-release
+
+RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
+
+USER root
+RUN apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+USER $USERNAME
+
