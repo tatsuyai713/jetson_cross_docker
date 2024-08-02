cd2d871655065ba30cec794c69ed0d2974a4fe17
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-08-03 02:39:13 +0900

    Fix bug

--- build_image.sh
+++ build_image.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+sudo apt update
+sudo apt install -y qemu-user-static
+
 IMAGE_NAME="ghcr.io/tatsuyai713/jetson_cross_image"
 IMAGE_VERSION="5.1.2"
 
@@ -10,4 +13,4 @@ SCRIPT_DIR=$(pwd)
 USER_IMAGE_NAME="jetson_cross_image_$USER"
 
 echo "Build Container"
-docker build --file=./noproxy.dockerfile -t $USER_IMAGE_NAME . --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=$USER
+docker build --file=./noproxy.dockerfile -t $USER_IMAGE_NAME . --platform=linux/arm64 --build-arg UID=$(id -u) --build-arg GID=$(id -g) --build-arg UNAME=$USER
--- launch_container_no_nvidia.sh
+++ launch_container_no_nvidia.sh
@@ -3,16 +3,39 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 
 NAME_IMAGE="jetson_cross_image_$USER"
+DOCKER_NAME="jetson_cross_docker_$USER"
 
 # Commit
 if [ ! $# -ne 1 ]; then
 	if [ "commit" = $1 ]; then
 		echo 'Now commiting docker container...'
-		docker commit jetson_cross_docker $NAME_IMAGE:latest
-		CONTAINER_ID=$(docker ps -a -f name=jetson_cross_docker --format "{{.ID}}")
+		docker commit $DOCKER_NAME $NAME_IMAGE:latest
+		CONTAINER_ID=$(docker ps -a -f name=$DOCKER_NAME --format "{{.ID}}")
 		docker rm $CONTAINER_ID
 		exit
 	fi
+fi
+
+# Stop
+if [ ! $# -ne 1 ]; then
+    if [ "stop" = $1 ]; then
+        CONTAINER_ID=$(docker ps -a -f name=$DOCKER_NAME --format "{{.ID}}")
+        docker stop $CONTAINER_ID
+        docker rm $CONTAINER_ID -f
+        exit 0
+    fi
+fi
+
+# Delete
+if [ ! $# -ne 1 ]; then
+    if [ "delete" = $1 ]; then
+        echo 'Now deleting docker container...'
+        CONTAINER_ID=$(docker ps -a -f name=$DOCKER_NAME --format "{{.ID}}")
+        docker stop $CONTAINER_ID
+        docker rm $CONTAINER_ID -f
+        docker image rm $NAME_IMAGE
+        exit 0
+    fi
 fi
 
 XAUTH=/tmp/.docker.xauth
@@ -24,7 +47,6 @@ fi
 chmod a+r $XAUTH
 
 DOCKER_OPT=""
-DOCKER_NAME="jetson_cross_docker"
 DOCKER_WORK_DIR="/home/${USER}"
 
 ## For XWindow
@@ -40,21 +62,19 @@ DOCKER_OPT="${DOCKER_OPT} \
         --hostname `hostname`-Docker \
         --add-host `hostname`-Docker:127.0.1.1"
 
-# For nvidia-docker
-# DOCKER_OPT="${DOCKER_OPT} --runtime=nvidia "
-# DOCKER_OPT="${DOCKER_OPT} --privileged -it "
+DOCKER_OPT="${DOCKER_OPT} --privileged -it "
 
 ## Allow X11 Connection
 xhost +local:`hostname`-Docker
-CONTAINER_ID=$(docker ps -a -f name=jetson_cross_docker --format "{{.ID}}")
+CONTAINER_ID=$(docker ps -a -f name=$DOCKER_NAME --format "{{.ID}}")
 if [ ! "$CONTAINER_ID" ]; then
 	docker run ${DOCKER_OPT} \
 		--volume=/dev:/dev:rw \
-		--shm-size=1gb \
+		--shm-size=4gb \
 		--env=TERM=xterm-256color \
 		--name=${DOCKER_NAME} \
 		$NAME_IMAGE:latest \
-		bash
+		/bin/bash
 else
 	docker start $CONTAINER_ID
 	docker attach $CONTAINER_ID
